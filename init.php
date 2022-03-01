3fd883c12c0a9da8f319aa46b21fee6a4a97b21f
JAIMIN-CHOKHAWALA <jaimin.chokhawala@gmail.com>
2022-03-02 03:29:56 +0530

    adding .env file for environment variables of application

--- init.php
+++ init.php
@@ -2,13 +2,18 @@
 
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/routes.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 
 /* App class/Model loading */
 // automatic class load
 function auto_class_load(string $class_name) {
-  if (preg_match('/\A\w+\Z/', $class_name)) {
-    require_once('classes/' . $class_name . '.class.php');
-  }
+    if (preg_match('/\A\w+\Z/', $class_name)) {
+        require_once('classes/' . $class_name . '.class.php');
+    }
 }
 spl_autoload_register('auto_class_load');
 
@@ -16,10 +21,10 @@ spl_autoload_register('auto_class_load');
  * App database connection
  */
 $db = [
-    'host' => DB_HOST,
-    'user' => DB_USER,
-    'password' => DB_PASS,
-    'name' => DB_NAME
+    'host' => $_ENV['DB_HOST'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS'],
+    'name' => $_ENV['DB_NAME']
 ];
 
 DB::set_database(
